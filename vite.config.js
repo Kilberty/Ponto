f2a150a1ee56beab07dd4a905a0ad2bfd72795b2
Kilberty <kilberty1995@gmail.com>
2025-08-21 07:55:12 -0300

    ajuste requisicao

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    build: {
-        outDir: 'public', // O Vite irá gerar os arquivos diretamente na pasta 'public'
-    },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
             refresh: true,
-            publicDirectory: 'public', // Garante que o Laravel saiba a pasta de build
         }),
         react(),
     ],
