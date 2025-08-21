9ce5cde43a91f18f86443c5e994ab42a5ffe3f57
Kilberty <kilberty1995@gmail.com>
2025-08-21 08:39:10 -0300

    ajuste rotas

--- routes/web.php
+++ routes/web.php
@@ -31,14 +31,14 @@ Route::get('/register',function(){
 
 Route::middleware('auth')->group(function () {
    Route::get('/empresa',[BusinessController::class,'index'])->name('empresa');
-   Route::get('/ufs',[UfController::class,'index'])->name('uf');
+   Route::get('/build/ufs',[UfController::class,'index'])->name('uf');
    Route::get('/funcionarios',[EmployeeController::class,'index'])->name('funcionarios');
    Route::post('/funcionarios/adicionar',[EmployeeController::class,'store']);
    Route::get('/funcionarios/gerarCodigo',[EmployeeController::class,'generate']);
    Route::get('/funcionarios/{employee}',[EmployeeController::class,'show'])->name('funcionarios.show');
    Route::patch('/funcionarios/{employee}',[EmployeeController::class,'update']);
    Route::post('/empresa/update',[BusinessController::class,'update']);
-   Route::get('/funcoes/autocomplete',[RoleController::class,'autocompleteNome']);
+   Route::get('/build/funcoes/autocomplete',[RoleController::class,'autocompleteNome']);
    Route::post('/funcoes/adicionar',[RoleController::class,'store']);
    Route::post('/funcoes/atualizar',[RoleController::class,'update']);
    Route::get('/funcoes',[RoleController::class,'index'])->name('funcoes');
@@ -46,7 +46,7 @@ Route::middleware('auth')->group(function () {
    Route::post('/obras/add',[ConstructionController::class,'store']);
    Route::patch('/obras/{construction}/descricao',[ConstructionController::class,'description']);
    Route::get('/obras/{construction}/descricao',[ConstructionController::class,'show']);
-   Route::get('/obras/autocomplete',[ConstructionController::class,'autoCompleteConstruction']);
+   Route::get('/build/obras/autocomplete',[ConstructionController::class,'autoCompleteConstruction']);
 });
 
 require __DIR__.'/auth.php';
