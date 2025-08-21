e63bde1e5feb38032e05d0b2de27391141a2f6ac
Kilberty <kilberty1995@gmail.com>
2025-08-20 21:42:13 -0300

    ajuste logo

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -2,7 +2,7 @@ export default function ApplicationLogo(props) {
     return (
         <img
             alt="Logo"
-            src="https://postimg.cc/WhHm29NG"
+            src="https://i.postimg.cc/P5tSQc0R/logo.png"
             className="w-20 h-20 object-contain"
             {...props}
         />
